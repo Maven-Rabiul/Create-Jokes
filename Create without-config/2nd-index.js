2674de9aa453e3278d87f7f04c67750f7cdec5d4
Md. Rabiul Alam <mavenrabiul@gmail.com>
2024-09-17 13:04:01 +0600

    files names change and JS done.
    files names has been changed to clear the confusion with other directories file name,,and javascript coding has been done..for now.

---
+++ Create without-config/2nd-index.js
@@ -0,0 +1,23 @@
+
+
+
+function dataLoaded() {
+    let tableBody = document.querySelector("#tbody");
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+        console.log(response.data);
+        let posts = response.data;
+
+        posts.slice(0,10).map((post) => {
+            tableBody.innerHTML = tableBody.innerHTML +`
+            <tr>
+            <td>${post.id}</td>
+            <td>${post.title}</td>
+            <td>${post.body}</td>
+            </tr>
+            `
+
+        })
+    })
+}
+
+window.onload = dataLoaded;
